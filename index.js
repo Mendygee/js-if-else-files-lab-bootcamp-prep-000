b0365fbb6ccd7add00890c66e0b353f4a32079db
Menachem Goorevitch <menachemgoorevitch@gmail.com>
2017-09-29 16:27:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var name = "Susan"
-var height = "70"
+var height = 70
 if (name === "Susan") {
   console.log ("the name is correct")
 } else {
   console.log ("I thought the name was Susan!")
 }
 
-if (height === "70") {
+if (height === 70) {
   console.log ("the height is correct")
 } else {
   console.log ("I thought her height was 70!")
